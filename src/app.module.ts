2de893b8c66fb6f05c0e0ff340e627df8a7bb62b
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-11-06 14:58:40 +0100

    Connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TripsModule } from './trips/trips.module';
 
 @Module({
-  imports: [TripsModule],
+  imports: [
+    TripsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'travel-nest',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
