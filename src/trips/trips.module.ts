d3e9852450bfdebf78efec69c9ce974604e7c588
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2022-11-15 23:30:31 +0100

    Update trips module

--- src/trips/trips.module.ts
+++ src/trips/trips.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Trip } from 'src/all-trips/all-trips.entity';
+import { AllTripsModule } from 'src/all-trips/all-trips.module';
 import { AuthModule } from 'src/auth/auth.module';
-import { Trip } from './trip.entity';
+import { Favourite } from './trip.entity';
 import { TripRepository } from './trip.repository';
 import { TripsController } from './trips.controller';
 import { TripsService } from './trips.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Trip]), AuthModule],
+  imports: [
+    TypeOrmModule.forFeature([Favourite]),
+    AuthModule,
+    TypeOrmModule.forFeature([Trip]),
+    AllTripsModule,
+  ],
   controllers: [TripsController],
   providers: [TripsService, TripRepository],
 })
