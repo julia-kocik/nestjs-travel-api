9bb0a237c35c56e8859649699ab9bd39f6a49274
juliakocik <julia.kocik02@gmail.com>
2023-06-26 22:00:44 +0200

    Add swagger config

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
+import { createDocument } from 'swagger/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
+  app.setGlobalPrefix('api/v1');
+  SwaggerModule.setup('api', app, createDocument(app));
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
