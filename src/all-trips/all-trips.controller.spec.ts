b82699d3a64168a4789db29c334922da9afdf165
juliakocik <julia.kocik02@gmail.com>
2023-07-01 16:42:36 +0200

    Add test for method getAllTrips in trips.controller

--- src/all-trips/all-trips.controller.spec.ts
+++ src/all-trips/all-trips.controller.spec.ts
@@ -2,7 +2,19 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AllTripsController } from './all-trips.controller';
 import { AllTripsService } from './all-trips.service';
 
-const mockAllTripsService = {};
+const mockAllTripsService = {
+  getAllTrips: jest.fn(),
+};
+
+const mockTrip = {
+  id: '1befbb09-782a-4924-9e51-98f8ec8069ee',
+  name: 'New York City Trip',
+  description: 'Beautiful memories of city that never sleeps.',
+  destination: 'NYC',
+  price: 40,
+  places: 20,
+  status: 'AVAILABLE',
+};
 
 describe('All Trips Controller', () => {
   let controller: AllTripsController;
@@ -26,5 +38,15 @@ describe('All Trips Controller', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
     expect(service).toBeDefined();
+  });
+  describe('getAllTrips', () => {
+    it('should successfully return allTrips', async () => {
+      jest
+        .spyOn(service, 'getAllTrips')
+        .mockImplementation(() => [mockTrip] as any);
+      const result = await controller.getAllTrips();
+      expect(service.getAllTrips).toHaveBeenCalled();
+      expect(result).toEqual([mockTrip]);
+    });
   });
 });
