69b83301a8783e366c75940497a4d5b71cd79915
juliakocik <julia.kocik02@gmail.com>
2023-07-02 10:55:57 +0200

    Add test for method getAllTrips in trips.controller

--- src/trips/trips.controller.spec.ts
+++ src/trips/trips.controller.spec.ts
@@ -2,8 +2,28 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TripsController } from './trips.controller';
 import { TripsService } from './trips.service';
 import { PassportModule } from '@nestjs/passport';
+import { User } from 'src/auth/user.entity';
 
-const mockTripsService = {};
+const mockTripsService = {
+  getAllTrips: jest.fn(),
+};
+
+const mockTrip = {
+  id: '1befbb09-782a-4924-9e51-98f8ec8069ee',
+  name: 'New York City Trip',
+  description: 'Beautiful memories of city that never sleeps.',
+  destination: 'NYC',
+  price: 40,
+  places: 20,
+  status: 'AVAILABLE',
+};
+
+const mockUser: User = {
+  id: '123',
+  username: 'user2',
+  password: 'Test1234',
+  trips: [],
+};
 
 describe('Trips Controller', () => {
   let controller: TripsController;
@@ -28,5 +48,16 @@ describe('Trips Controller', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
     expect(service).toBeDefined();
+  });
+
+  describe('getAllTrips', () => {
+    it('should successfully return allTrips', async () => {
+      jest
+        .spyOn(service, 'getAllTrips')
+        .mockImplementation(() => [mockTrip] as any);
+      const result = await controller.getAllTrips(mockUser);
+      expect(service.getAllTrips).toHaveBeenCalled();
+      expect(result).toEqual([mockTrip]);
+    });
   });
 });
