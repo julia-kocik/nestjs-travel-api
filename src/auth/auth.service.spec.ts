d78d60630d6aefa47f7ec7eefda818a22b27bc7d
juliakocik <julia.kocik02@gmail.com>
2023-07-02 20:06:15 +0200

    Add test for method signUp in auth.controller

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -2,14 +2,19 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { AuthService } from './Auth.service';
 import { UserRepository } from './user.repository';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as bcrypt from 'bcrypt';
 
-const mockUserRepository = {};
+const mockUserRepository = {
+  create: jest.fn(),
+  save: jest.fn(),
+};
 
 describe('AuthService', () => {
   let service: AuthService;
   let repository: UserRepository;
+  let jwtService: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -36,10 +41,57 @@ describe('AuthService', () => {
 
     service = module.get<AuthService>(AuthService);
     repository = module.get(getRepositoryToken(UserRepository));
+    jwtService = module.get<JwtService>(JwtService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
     expect(repository).toBeDefined();
+  });
+
+  describe('signup', () => {
+    it('should register a new user', async () => {
+      const mockCredentialsDto = { username: 'user123', password: 'password' };
+      const mockSalt = 'mockSalt';
+      const mockHashedPassword = 'mockHashedPassword';
+      const hashedUser = {
+        username: mockCredentialsDto.username,
+        password: mockHashedPassword,
+      };
+      const mockToken = '1234567';
+
+      const saltSpy = jest
+        .spyOn(bcrypt, 'genSalt')
+        .mockImplementation(() => mockSalt);
+      const hashSpy = jest
+        .spyOn(bcrypt, 'hash')
+        .mockImplementation(() => mockHashedPassword);
+      const createSpy = jest
+        .spyOn(repository, 'create')
+        .mockImplementation(() => hashedUser as any);
+      const saveSpy = jest
+        .spyOn(repository, 'save')
+        .mockImplementation(() => hashedUser as any);
+      const jwtSpy = jest
+        .spyOn(jwtService, 'sign')
+        .mockImplementation(() => mockToken as any);
+
+      const result = await service.signUp(mockCredentialsDto);
+
+      expect(saltSpy).toHaveBeenCalled();
+      expect(hashSpy).toHaveBeenCalledWith(
+        mockCredentialsDto.password,
+        mockSalt,
+      );
+      expect(createSpy).toHaveBeenCalledWith(hashedUser);
+      expect(saveSpy).toHaveBeenCalledWith(hashedUser);
+      expect(jwtSpy).toHaveBeenCalledWith({
+        username: mockCredentialsDto.username,
+      });
+
+      expect(result).toEqual({
+        accessToken: mockToken,
+      });
+    });
   });
 });
