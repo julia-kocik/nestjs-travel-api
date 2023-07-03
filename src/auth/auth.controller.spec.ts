61e664158a6cc7eb14dc87af9469d99c48aaf2d5
juliakocik <julia.kocik02@gmail.com>
2023-07-03 22:32:06 +0200

    Add testbasic test for auth.controller

---
+++ src/auth/auth.controller.spec.ts
@@ -0,0 +1,30 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+const mockAuthService = {};
+
+describe('Auth Controller', () => {
+  let controller: AuthController;
+  let service: AuthService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
+    }).compile();
+
+    controller = module.get<AuthController>(AuthController);
+    service = module.get<AuthService>(AuthService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+    expect(service).toBeDefined();
+  });
+});
