529e61ed6159971cafda8998116925c6536c9ebd
juliakocik <julia.kocik02@gmail.com>
2023-07-16 11:36:06 +0200

    Add test for AuthController signup method

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,8 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
-const mockAuthService = {};
+const mockAuthService = {
+  signUp: jest.fn(),
+};
 
 describe('Auth Controller', () => {
   let controller: AuthController;
@@ -26,5 +32,42 @@ describe('Auth Controller', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
     expect(service).toBeDefined();
+  });
+
+  describe('signup', () => {
+    it('should register a new user', async () => {
+      const mockCredentialsDto = { username: 'user123', password: 'password' };
+      const mockToken = '1234567';
+      const serviceSpy = jest
+        .spyOn(service, 'signUp')
+        .mockImplementation(() => ({ accessToken: mockToken } as any));
+      const result = await controller.createUser(mockCredentialsDto);
+      expect(serviceSpy).toHaveBeenCalledWith(mockCredentialsDto);
+      expect(result).toEqual({ accessToken: mockToken });
+    });
+
+    it('should throw ConflictException if error code is 23505', async () => {
+      const mockCredentialsDto = { username: 'user123', password: 'password' };
+
+      jest
+        .spyOn(service, 'signUp')
+        .mockImplementation(() => Promise.reject({ code: '23505' }));
+
+      await expect(controller.createUser(mockCredentialsDto)).rejects.toThrow(
+        new ConflictException('Username already exists'),
+      );
+    });
+
+    it('should throw InternalServerError if error code is NOT 23505', async () => {
+      const mockCredentialsDto = { username: 'user123', password: 'password' };
+
+      jest
+        .spyOn(service, 'signUp')
+        .mockImplementation(() => Promise.reject({ code: '' }));
+
+      await expect(controller.createUser(mockCredentialsDto)).rejects.toThrow(
+        new InternalServerErrorException(),
+      );
+    });
   });
 });
