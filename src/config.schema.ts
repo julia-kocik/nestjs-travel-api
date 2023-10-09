381f08feae640a7ab0469246b0f03d1270fa9a45
julia-kocik <julia.kocik@gmail.com>
2023-10-09 22:21:42 +0200

    Revert "Update config"
    This reverts commit 03c190d12eab54d95f33c03efcb650501eddb368.

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -2,11 +2,11 @@ import * as Joi from '@hapi/joi';
 
 export const configValidationSchema = Joi.object({
   PORT: Joi.number().default(3000),
-    STAGE: Joi.string(),
+    STAGE: Joi.string().required(),
     DATABASE_URL: Joi.string(),
     DB_PORT: Joi.number().default(5432),
-    POSTGRES_USER: Joi.string(),
-    POSTGRES_PASSWORD: Joi.string(),
-    POSTGRES_DB: Joi.string(),
-    JWT_SECRET: Joi.string(),
+    POSTGRES_USER: Joi.string().required(),
+    POSTGRES_PASSWORD: Joi.string().required(),
+    POSTGRES_DB: Joi.string().required(),
+    JWT_SECRET: Joi.string().required(),
 });
