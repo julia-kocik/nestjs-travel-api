220d37950e25161b9385cf5db165d4a994363388
julia-kocik <julia.kocik@gmail.com>
2023-11-08 16:38:43 +0100

    Update swagger description

--- swagger/swagger.config.ts
+++ swagger/swagger.config.ts
@@ -2,7 +2,7 @@ import { SwaggerConfig } from './swagger.interface';
 
 export const SWAGGER_CONFIG: SwaggerConfig = {
   title: 'Nest js Trips Api',
-  description: 'Nest js api allows to create new trips with /all-trips, create account and login with /signup and /signin and save favourite trips after authorization with /favourites.',
+  description: 'Nest js Trips api allows to create new trips with /all-trips, create account and login with /signup and /signin and save favourite trips after authorization with /favourites.',
   version: '1.0',
   tags: [''],
 };
